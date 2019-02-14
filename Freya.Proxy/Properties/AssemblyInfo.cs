e57256b8869a8cada45157dbc087e78bd641bd05
Angeloeyez <gavenhuang@gmail.com>
2019-02-14 15:48:46 +0800

    Enable service radio

--- Freya.Proxy/Properties/AssemblyInfo.cs
+++ Freya.Proxy/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，或將組建編號或修訂編號設為預設值
 //方法是使用 '*'，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2019.2.14.0738")]
-[assembly: AssemblyFileVersion("2019.2.14.0738")]
+[assembly: AssemblyVersion("2019.2.14.0748")]
+[assembly: AssemblyFileVersion("2019.2.14.0748")]
